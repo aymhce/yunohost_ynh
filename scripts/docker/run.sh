3076ba93752805b3d300de475b8db8aa46279445
aymhce <aymhce@gmail.com>
2018-02-26 22:41:55 +0100

    version 2.7.9 + upgrade dockerapp-yunohost

--- scripts/docker/run.sh
+++ scripts/docker/run.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-image=domainelibre/yunohost2:2.7.7-1
-[ "$architecture" == "i386" ]  && image=domainelibre/yunohost2-i386:2.7.7-1
-[ "$architecture" == "armhf" ] && image=domainelibre/yunohost2-arm:2.7.7-1
+[ "$architecture" == "amd64" ] && image=domainelibre/yunohost2:2.7.9-1
+[ "$architecture" == "i386" ]  && image=domainelibre/yunohost2-i386:2.7.9-1
+[ "$architecture" == "armhf" ] && image=domainelibre/yunohost2-arm:2.7.9-1
+[ -z $image ] && ynh_die "Sorry, your $architecture architecture is not supported ..."
 
-options="-p $port:443 -v $data_path/data:/home/yunohost.backup"
+options="-p $port:443 -v $data_path/data:/home/yunohost.backup -v /var/run/docker.sock:/var/run/docker.sock"
 
 docker run -d -h yunohost.DOMAIN --name=$app \
  --privileged \
  --restart always \
  -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
- $options $image /bin/systemd >/dev/null 2>&1
+ $options $image /bin/systemd 1>&2
 echo $?
